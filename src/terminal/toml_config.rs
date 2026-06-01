e5a5c711211b151c223610d3a2ddfe8a1d267850
thetrueryan <georgytoper@gmail.com>
2026-06-01 14:03:08 +0400

    add toml deserialize method to Config

--- src/terminal/toml_config.rs
+++ src/terminal/toml_config.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use std::error::Error;
+
 use serde::Deserialize;
 
 use crate::world::config::WorldConfig;
@@ -5,20 +8,35 @@ use crate::world::config::WorldConfig;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    world: WorldConfig,
-    ai: AIConfig,
-    terminal: TerminalConfig
+    pub world: WorldConfig,
+    pub ai: AIConfig,
+    pub terminal: TerminalConfig
 }
 
 
 #[derive(Deserialize, Debug)]
 pub struct AIConfig {
-    ai_count: u16
+    pub ai_count: u16
 }
 
 
 #[derive(Deserialize, Debug)]
 pub struct TerminalConfig {
-    offset_x: u16,
-    offset_y: u16
+    pub offset_x: u16,
+    pub offset_y: u16
+}
+
+
+impl Config {
+    pub fn from_toml(file: String) -> Result<Self, Box<dyn Error>> {
+        let exe = std::env::current_exe()?;
+        let dir = exe
+            .parent().unwrap()
+            .parent().unwrap()
+            .parent().unwrap();
+        let path = dir.join(file);
+        let s = fs::read_to_string(path).expect(dir.to_str().unwrap());
+        let config: Config = toml::from_str(&s)?;
+        Ok(config)
+    }
 }
